5baa3a275d8c5ebc00d2ff941b5aaa15e87dc089
Liza Babych <bettyhetfield11951@gmail.com>
2020-09-24 19:26:46 +0300

    Add validation

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,22 +1,76 @@
-import React from 'react';
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 export const Login = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email("Поле должно быть email адрессом").required("Это поле обязательное"),
+      password: Yup.string().min(6, "Количество символов меньше 6").required("Это поле обязательное!"),
+    }),
+    onSubmit: async (values) => {
+      try {
+        let response = await fetch("http://localhost:5000/v1/login", {
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/json',
+            'charset': 'utf-8',
+          },
+          body: JSON.stringify({
+            "email": values.email,
+            "password": values.password,
+          }),
+        });
+        if (!response.ok) {
+            console.log("Error: " + response.status);
+        }
+        let res = await response.json();
+        console.log(res);
+      } catch (error) {
+          alert("Error");
+      }
+   },
+  });
   return (
-    <div className="container">
-      <div className="form">
-        <p className="text-mut">Вход в MyTrello</p>
-        <form>
-          <div className="form-group">
-            <input className="form-control" placeholder="Укажите адресс электронной почты"/>
-          </div>
-          <div className="form-group">
-            <input className="form-control" placeholder="Укажите пароль"/>
-          </div>
-          <div className="center">
-            <button type="submit" className="btn btn-success">Войти</button>
-          </div>
-        </form>
-      </div>
+    <div className="form">
+      <p className="text-mut">Вход в MyTrello</p>
+      <form onSubmit={formik.handleSubmit} noValidate>
+        <div className="form-group">
+          <input
+            type="email"
+            name="email"
+            className="form-control"
+            placeholder="Укажите email"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          {formik.touched.email && formik.errors.email && (
+           <div className="text-danger">{formik.errors.email}</div>
+         )}
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            name="password"
+            className="form-control"
+            placeholder="Укажите пароль"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          {formik.touched.password && formik.errors.password && (
+           <div className="text-danger">{formik.errors.password}</div>
+         )}
+        </div>
+        <div className="center">
+          <button type="submit" className="btn btn-success">
+            Войти
+          </button>
+        </div>
+      </form>
     </div>
   );
-}
+};
